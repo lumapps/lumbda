d9a66edb9f0e25899f42068e40c778be341c7c44
Florent Espanet <florent@lumapps.com>
2016-03-29 10:14:00 +0200

    Update the package url

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ setup(
     long_description=open(join(dirname(__file__), 'README.md')).read(),
     author=lumbda.__author__,
     author_email=lumbda.__author_email__,
+    url='https://github.com/lumapps/lumbda',
     license=open(join(dirname(__file__), 'LICENSE')).read(),
     keywords=['utilities', 'utils'],
     install_requires=[],
